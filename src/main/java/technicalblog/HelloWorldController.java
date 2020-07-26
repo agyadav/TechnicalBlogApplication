477bfb1892a689cbcaca7eb1e3852c63cea590bc
anand <anayadav@adobe.com>
2020-07-26 20:43:40 +0530

    First milestone

---
+++ src/main/java/technicalblog/HelloWorldController.java
@@ -0,0 +1,14 @@
+package technicalblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping("/hellospring")
+    @ResponseBody
+    public String helloSpring(){
+        return "Welcome to Technical Blog";
+    }
+}
